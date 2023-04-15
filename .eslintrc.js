a2916c795fe242755be8fecb5fcbd6b8135cc5f9
Miguel Parramón Teixidó <mparramont@gmail.com>
2023-04-15 18:28:42 +0800

    build: fix the .tsx extension thingie the right way

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,23 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
-  overrides: [
-    {
-      files: ['*.tsx'],
-      rules: {
-        'react/jsx-filename-extension': [
-          1,
-          {
-            extensions: ['.tsx']
-          }
-        ]
-      }
-    }
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier'
   ],
+  globals: {
+    React: 'readonly'
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: './tsconfig.json',
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'error'
+    '@typescript-eslint/no-explicit-any': 'error',
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Root from './routes/root.tsx'
+import Root from './routes/root'
 
 const router = createBrowserRouter([
   {
