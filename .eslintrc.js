bc1b0aaccf79d9a341f52c253d00f8a24a042a47
Miguel Parramón Teixidó <mparramont@gmail.com>
2023-04-15 18:29:51 +0800

    feat: add error page

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
   plugins: ['react', '@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'error',
+    'no-console': ['error', { allow: ['error'] }]
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Root from './routes/root'
+import ErrorPage from './error-page'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root />
+    element: <Root />,
+    errorElement: <ErrorPage />
   }
 ])
 
