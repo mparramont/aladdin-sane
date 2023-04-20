6dce4331591afc45178de2e0853e069365d9bfc6
Miguel Parramón Teixidó <miguel@abillion.com>
2023-04-20 20:41:07 +0800

    test: change album test to e2e by using playwright

--- src/routes/album.test.tsx
+++ src/routes/album.test.tsx
@@ -1,26 +1,28 @@
-import { render } from '@testing-library/react'
-import React from 'react'
-import { RouterProvider, createMemoryRouter } from 'react-router-dom'
-import userEvent from '@testing-library/user-event'
-import { routes } from '../routes'
-
-const router = createMemoryRouter(routes)
+import { chromium } from 'playwright'
+import { test, expect } from '@playwright/test'
 
 test('renders album information', async () => {
-  const { getByText } = render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
+  // setup
+  const browser = await chromium.launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+
+  // navigation
+  await page.goto('http://localhost:3000/')
+  await page.click('text=Heroes')
+
+  // route test
+  const albumTitle = page.getByText(
+    'David Bowie (aka Space Oddity) [2015 Remaster]'
   )
-  // FIXME this is testing more than the unit under test, since it's also testing the root component by clicking on it.
-  // TODO test the root component separately
-  await userEvent.click(getByText('David Bowie (1969)'))
+  const artistName = page.getByText('David Bowie')
+  const summary = page.getByText(
+    /David Bowie \(commonly known as Space Oddity\)/i
+  )
+  expect(albumTitle).not.toBeNull()
+  expect(artistName).not.toBeNull()
+  expect(summary).not.toBeNull()
 
-  const albumTitle = getByText('David Bowie (aka Space Oddity) [2015 Remaster]')
-  const artistName = getByText('David Bowie')
-  const summary = getByText(/David Bowie \(commonly known as Space Oddity\)/i)
-
-  expect(albumTitle).toBeInTheDocument()
-  expect(artistName).toBeInTheDocument()
-  expect(summary).toBeInTheDocument()
+  // teardown
+  await browser.close()
 })
